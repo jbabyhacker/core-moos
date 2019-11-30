b19791e5adb35d9c5c5115c2ba073b28b0793c04
jbabyhacker <jasoneklein5@gmail.com>
2019-11-30 11:57:32 -0500

    Initial commit

---
+++ Core/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp_c.cpp
@@ -0,0 +1,60 @@
+//
+// Created by jklein on 11/30/19.
+//
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp_c.h"
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+#include <functional>
+
+using IterateCallback = std::function<bool(void)>;
+using OnStartUpCallback = std::function<bool(void)>;
+
+class RustMoosApp : public AppCastingMOOSApp {
+public:
+    void setIterateCallback(IterateCallback callback) {
+        m_iterateCallback = callback;
+    }
+
+    void setOnStartUpCallback(OnStartUpCallback callback) {
+        m_onStartUpCallback = callback;
+    }
+
+protected:
+    bool Iterate() override {
+        if(m_iterateCallback) {
+            return m_iterateCallback();
+        } else {
+            return false;
+        }
+    }
+
+    bool OnStartUp() override {
+        if(m_onStartUpCallback) {
+            return m_onStartUpCallback();
+        } else {
+            return false;
+        }
+    }
+
+private:
+    IterateCallback m_iterateCallback;
+    OnStartUpCallback  m_onStartUpCallback;
+};
+
+extern "C" {
+RustMoosApp* newRustMoosApp() {
+    return new RustMoosApp();
+}
+
+void deleteRustMoosApp(RustMoosApp* v) {
+    delete v;
+}
+
+void RustMoosApp_setIterateCallback(RustMoosApp* v, rust_callback callback) {
+    v->setIterateCallback(callback);
+}
+
+void RustMoosApp_setOnStartUpCallback(RustMoosApp *v, rust_callback callback) {
+    v->setOnStartUpCallback(callback);
+}
+}
---
+++ Core/libMOOS/Thirdparty/AppCasting/include/MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp_c.h
@@ -0,0 +1,26 @@
+//
+// Created by jklein on 11/30/19.
+//
+
+#ifndef MOOS_APPCASTINGMOOSAPP_C_H
+#define MOOS_APPCASTINGMOOSAPP_C_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    typedef struct RustMoosApp RustMoosApp;
+    typedef bool (*rust_callback)(void);
+
+    RustMoosApp* newRustMoosApp();
+
+    void deleteRustMoosApp(RustMoosApp* v);
+
+    void RustMoosApp_setIterateCallback(RustMoosApp* v, rust_callback callback);
+
+    void RustMoosApp_setOnStartUpCallback(RustMoosApp *v, rust_callback callback);
+
+#ifdef __cplusplus
+};
+#endif
+#endif //MOOS_APPCASTINGMOOSAPP_C_H
